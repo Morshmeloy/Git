8d280b2f0bdbf98a8e547bcad5458e1706270728
Morshmeloy <gugolgol@mail.ru>
2020-12-11 23:44:21 +0300

    Last Words

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("What's up Bro ?");
         System.out.println("Normal Men,just Ok");
+        System.out.println("Good");
     }
 }
