c3c3c32c07d4b161652151acd823576d3cbed5db
Morshmeloy <gugolgol@mail.ru>
2020-12-12 15:16:12 +0300

    let's start again

--- src/main/java/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("What's up Bro ?");
-        System.out.println("Normal Men,just Ok");
-    }
-}
